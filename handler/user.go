7ea596756eb6c691b62d1bab395e01e435464523
Metilda Chee Heng Er <metilda.cheehe@shopee.com>
2021-07-03 09:25:31 +0800

    Combine to one file for user.go

--- handler/user.go
+++ handler/user.go
@@ -12,6 +12,21 @@ import (
 	"github.com/metildachee/userie/model"
 )
 
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	users, err := dao.GetUsers()
+	if err != nil {
+		// todo: return err code in response
+		log.Print(fmt.Sprintf("get users from dao err=%s", err), ERROR)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	if err := json.NewEncoder(w).Encode(users); err != nil {
+		log.Print(fmt.Sprintf("get users json encoder err=%s", err), ERROR)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
 func GetUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
