8f6d70ca47981a8106dc0336475f6ee246061488
Metilda Chee Heng Er <metilda.cheehe@shopee.com>
2021-07-03 18:07:35 +0800

    Add create user function

--- dao/dao.go
+++ dao/dao.go
@@ -1,16 +1,26 @@
 package dao
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/elastic/go-elasticsearch"
+	"github.com/elastic/go-elasticsearch/esapi"
 	"github.com/metildachee/userie/logger"
 	"github.com/metildachee/userie/model"
 )
 
 type Dao struct {
-	cli *elasticsearch.Client
+	cli     *elasticsearch.Client
+	cluster string
+}
+
+func NewDao() *Dao {
+	return &Dao{}
 }
 
 func (dao *Dao) Init() error {
@@ -20,15 +30,22 @@ func (dao *Dao) Init() error {
 		return err
 	}
 	dao.cli = es
-
-	res, err := dao.cli.Info()
-	if err != nil {
-		logger.Print(fmt.Sprintf("failed to get init info from es end point %s", err), ERROR)
-		return err
-	}
-
-	logger.Print(fmt.Sprintf("init es client successfully %s", res), INFO)
+	dao.cluster = "usersg0"
+	logger.Print(fmt.Sprintf("init es client successfull"), INFO)
 	return nil
+}
+
+func (dao *Dao) CheckInit() bool {
+	if dao.cli == nil {
+		return false
+	}
+	_, err := dao.cli.Info()
+	if err != nil {
+		logger.Print(fmt.Sprintf("es client is not inited %s", err), INFO)
+		return false
+	}
+	logger.Print(fmt.Sprintf("es client is inited"), INFO)
+	return true
 }
 
 func (dao *Dao) GetUsers() (users []model.User, err error) {
@@ -60,8 +77,42 @@ func (dao *Dao) GetUser(userId int32) (user model.User, err error) {
 }
 
 func (dao *Dao) CreateUser(new model.User) (err error) {
+	if !dao.CheckInit() {
+		logger.Print(fmt.Sprintf("es client is not inited %s", err), ERROR)
+		return errors.New("es client not initialised")
+	}
+
+	req, err := dao.GetIndexRequest(new)
+	if err != nil {
+		logger.Print(fmt.Sprintf("%s", err), ERROR)
+		return
+	}
+
+	res, err := req.Do(context.Background(), dao.cli)
+	if err != nil {
+		logger.Print(fmt.Sprintf("%s", err), ERROR)
+		return
+	}
+	defer res.Body.Close()
+	if res.IsError() {
+		logger.Print(fmt.Sprintf("[%s] error indexing doc", res.Status()), ERROR)
+		return errors.New("error while indexing document")
+	}
 	return
-	// todo: upsert into es
+}
+
+func (dao *Dao) GetIndexRequest(doc model.User) (*esapi.IndexRequest, error) {
+	userDoc, err := json.Marshal(doc)
+	if err != nil {
+		logger.Print(fmt.Sprintf("json marshal failed=%s", err), ERROR)
+		return nil, err
+	}
+
+	return &esapi.IndexRequest{
+		Index:   dao.cluster,
+		Body:    strings.NewReader(string(userDoc)),
+		Refresh: "true",
+	}, nil
 }
 
 func (dao *Dao) UpdateUser(id int32, new model.User) (err error) {
--- dao/dao_test.go
+++ dao/dao_test.go
@@ -2,7 +2,10 @@ package dao
 
 import (
 	"testing"
+	"time"
 
+	"github.com/metildachee/userie/model"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -10,4 +13,22 @@ func TestInit(t *testing.T) {
 	dao := Dao{}
 	err := dao.Init()
 	require.Nil(t, err, "initialise es client should not have error")
+}
+
+func TestCreateUser(t *testing.T) {
+	u := model.User{
+		ID:          1,
+		Name:        "metchee",
+		DOB:         int32(time.Now().Unix()),
+		Address:     "Kent Ridge",
+		Description: "default user info",
+		Ctime:       int32(time.Now().Unix()),
+	}
+
+	dao := NewDao()
+	err := dao.Init()
+	assert.Nil(t, err, "should not have error when init")
+
+	err = dao.CreateUser(u)
+	assert.Nil(t, err, "should not have error when indexing doc")
 }
